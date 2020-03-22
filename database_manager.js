1a4db707548a507a119e35cf16c5cd7244e976d3
Ido Hayun <eidoil32@gmail.com>
2020-03-22 14:56:47 +0200

    add env variable for MongoDB

--- database_manager.js
+++ database_manager.js
@@ -2,7 +2,7 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
  
 // Connection URL
-const uri = "mongodb://heroku_bh9rr17h:kitchen_assistant3@ds119060.mlab.com:19060/heroku_bh9rr17h";
+const uri = ENV['MONGODB_URI'];
 const client = new MongoClient(uri);
 
 
