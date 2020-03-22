0df987383652bb3255146f06390055224d910153
Ido Hayun <eidoil32@gmail.com>
2020-03-22 14:22:35 +0200

    connect to db failed

--- database_manager.js
+++ database_manager.js
@@ -2,14 +2,15 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
  
 // Connection URL
-const uri = "mongodb://heroku_bh9rr17h:!2BJqy4eCyxXBcW@ds119060.mlab.com:19060/heroku_bh9rr17h";
+const uri = "mongodb://heroku_bh9rr17h:kitchen_assistant3@ds119060.mlab.com:19060/heroku_bh9rr17h";
+const client = new MongoClient(uri);
+client.connect();
+// // Use connect method to connect to the server
+// MongoClient.connect(uri, function(err, client) {
+//   assert.equal(null, err);
+//   console.log("Connected successfully to server");
  
-// Use connect method to connect to the server
-MongoClient.connect(uri, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+//   const db = client.db(dbName);
  
-  const db = client.db(dbName);
- 
-  client.close();
-});
+//   client.close();
+// });
